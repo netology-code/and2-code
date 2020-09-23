54692bf9df99217fb304aa90695aa6f2d73e0d06
coursar <coursar@gmail.com>
2020-09-23 14:23:53 +0300

    feat(crud)

---
+++ 07_crud/app/src/main/java/ru/netology/nmedia/util/AndroidUtils.kt
@@ -0,0 +1,12 @@
+package ru.netology.nmedia.util
+
+import android.content.Context
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+
+object AndroidUtils {
+    fun hideKeyboard(view: View) {
+        val imm = view.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+}
