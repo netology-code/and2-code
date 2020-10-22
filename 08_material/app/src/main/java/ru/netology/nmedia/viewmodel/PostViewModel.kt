76c1c62423ec7ff26fceed701e7d314a6b45a2cd
coursar <coursar@gmail.com>
2020-10-22 20:29:07 +0300

    feat(material)

---
+++ 08_material/app/src/main/java/ru/netology/nmedia/viewmodel/PostViewModel.kt
@@ -0,0 +1,44 @@
+package ru.netology.nmedia.viewmodel
+
+import androidx.lifecycle.*
+import ru.netology.nmedia.dto.Post
+import ru.netology.nmedia.repository.PostRepository
+import ru.netology.nmedia.repository.PostRepositoryInMemoryImpl
+
+private val empty = Post(
+    id = 0,
+    content = "",
+    author = "",
+    likedByMe = false,
+    likes = 0,
+    published = ""
+)
+
+class PostViewModel : ViewModel() {
+    // упрощённый вариант
+    private val repository: PostRepository = PostRepositoryInMemoryImpl()
+    val data = repository.getAll()
+    val edited = MutableLiveData(empty)
+
+    fun save() {
+        edited.value?.let {
+            repository.save(it)
+        }
+        edited.value = empty
+    }
+
+    fun edit(post: Post) {
+        edited.value = post
+    }
+
+    fun changeContent(content: String) {
+        val text = content.trim()
+        if (edited.value?.content == text) {
+            return
+        }
+        edited.value = edited.value?.copy(content = text)
+    }
+
+    fun likeById(id: Long) = repository.likeById(id)
+    fun removeById(id: Long) = repository.removeById(id)
+}
