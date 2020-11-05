9a204b7e561153bfc20d43980ab7145b1257ed44
coursar <coursar@gmail.com>
2020-11-05 22:05:09 +0300

    feat(push-sender)

---
+++ push-sender/src/main/kotlin/ru/netology/pusher/main.kt
@@ -0,0 +1,31 @@
+package ru.netology.pusher
+
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseOptions
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.Message
+import java.io.FileInputStream
+
+
+fun main() {
+    val options = FirebaseOptions.builder()
+        .setCredentials(GoogleCredentials.fromStream(FileInputStream("fcm.json")))
+        .setDatabaseUrl(dbUrl)
+        .build()
+
+    FirebaseApp.initializeApp(options)
+
+    val message = Message.builder()
+        .putData("action", "LIKE")
+        .putData("content", """{
+          "userId": 1,
+          "userName": "Vasiliy",
+          "postId": 2,
+          "postAuthor": "Netology"
+        }""".trimIndent())
+        .setToken(token)
+        .build()
+
+    FirebaseMessaging.getInstance().send(message)
+}
