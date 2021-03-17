27f720108c8033e5f6381a5f8cfe9c78c7c034e2
Anatoly Spitchenko <anatolij.sp@yandex.ru>
2021-03-17 15:22:29 +0300

    fix(recyclerview): trailing lambda

--- 06_recyclerview/recyclerview/app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
+++ 06_recyclerview/recyclerview/app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
@@ -3,10 +3,8 @@ package ru.netology.nmedia.activity
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import ru.netology.nmedia.R
 import ru.netology.nmedia.adapter.PostsAdapter
 import ru.netology.nmedia.databinding.ActivityMainBinding
-import ru.netology.nmedia.databinding.CardPostBinding
 import ru.netology.nmedia.viewmodel.PostViewModel
 
 class MainActivity : AppCompatActivity() {
@@ -20,8 +18,8 @@ class MainActivity : AppCompatActivity() {
             viewModel.likeById(it.id)
         }
         binding.list.adapter = adapter
-        viewModel.data.observe(this, { posts ->
+        viewModel.data.observe(this) { posts ->
             adapter.list = posts
-        })
+        }
     }
 }
