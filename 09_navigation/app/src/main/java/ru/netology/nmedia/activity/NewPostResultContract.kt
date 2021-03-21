54139f0c33ab941cec7ccc0559009dcdd222b01e
Anatoly Spitchenko <anatolij.sp@yandex.ru>
2021-03-21 13:34:01 +0300

    feature(navigation): activity result api

---
+++ 09_navigation/app/src/main/java/ru/netology/nmedia/activity/NewPostResultContract.kt
@@ -0,0 +1,19 @@
+package ru.netology.nmedia.activity
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import androidx.activity.result.contract.ActivityResultContract
+
+class NewPostResultContract : ActivityResultContract<Unit, String?>() {
+
+    override fun createIntent(context: Context, input: Unit?): Intent =
+        Intent(context, NewPostActivity::class.java)
+
+    override fun parseResult(resultCode: Int, intent: Intent?): String? =
+        if (resultCode == Activity.RESULT_OK) {
+            intent?.getStringExtra(Intent.EXTRA_TEXT)
+        } else {
+            null
+        }
+}
