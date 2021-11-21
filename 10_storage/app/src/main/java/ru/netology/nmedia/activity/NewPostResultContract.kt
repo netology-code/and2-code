669b9a25e4e88616449aba597d5e2baa72d5d6b9
Artem Mostyaev <reistlin12@gmail.com>
2021-11-21 12:44:00 +0300

    update(10_storage): the project as per Android Studio empty template

--- 10_storage/app/src/main/java/ru/netology/nmedia/activity/NewPostResultContract.kt
+++ 10_storage/app/src/main/java/ru/netology/nmedia/activity/NewPostResultContract.kt
@@ -7,7 +7,7 @@ import androidx.activity.result.contract.ActivityResultContract
 
 class NewPostResultContract : ActivityResultContract<Unit, String?>() {
 
-    override fun createIntent(context: Context, input: Unit?): Intent =
+    override fun createIntent(context: Context, input: Unit): Intent =
         Intent(context, NewPostActivity::class.java)
 
     override fun parseResult(resultCode: Int, intent: Intent?): String? =
