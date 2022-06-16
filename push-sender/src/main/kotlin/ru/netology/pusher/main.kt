1dada9a10a2546dd54de039fbbf66ce669b35033
jkuzmina <ju.a.kuzmina@yandex.ru>
2022-06-16 16:05:50 +0400

    add new post action

--- push-sender/src/main/kotlin/ru/netology/pusher/main.kt
+++ push-sender/src/main/kotlin/ru/netology/pusher/main.kt
@@ -7,21 +7,31 @@ import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.messaging.Message
 import java.io.FileInputStream
 
-
 fun main() {
+    println(token)
+
     val options = FirebaseOptions.builder()
         .setCredentials(GoogleCredentials.fromStream(FileInputStream("fcm.json")))
         .build()
 
     FirebaseApp.initializeApp(options)
 
-    val message = Message.builder()
-        .putData("action", "LIKE")
+    /*val message = Message.builder()
+        .putData("action", "LIKE1")
         .putData("content", """{
           "userId": 1,
           "userName": "Vasiliy",
           "postId": 2,
           "postAuthor": "Netology"
+        }""".trimIndent())
+        .setToken(token)
+        .build()*/
+
+    val message = Message.builder()
+        .putData("action", "NEWPOST")
+        .putData("content", """{
+          "userName": "Vasiliy",
+          "content": "Вам необходимо проанализировать, что произойдёт, если в приложение придёт Notification, у которого поле action не соответствует ни одному значению из Enum'а Action"
         }""".trimIndent())
         .setToken(token)
         .build()
