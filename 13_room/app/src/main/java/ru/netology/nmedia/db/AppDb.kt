71af5a6bcd859dd3f3101e4debfe3531bc738f00
Anatoly Spitchenko <anatolij.sp@yandex.ru>
2023-01-28 20:11:39 +0700

    room: add schema generation

--- 13_room/app/src/main/java/ru/netology/nmedia/db/AppDb.kt
+++ 13_room/app/src/main/java/ru/netology/nmedia/db/AppDb.kt
@@ -7,7 +7,7 @@ import androidx.room.RoomDatabase
 import ru.netology.nmedia.dao.PostDao
 import ru.netology.nmedia.entity.PostEntity
 
-@Database(entities = [PostEntity::class], version = 1)
+@Database(entities = [PostEntity::class], version = 1, exportSchema = true)
 abstract class AppDb : RoomDatabase() {
     abstract fun postDao(): PostDao
 
