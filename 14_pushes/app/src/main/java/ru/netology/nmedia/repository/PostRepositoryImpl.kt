2131d8a0218a2c16a461a690e1d7920bbe0c9ad2
Artem Mostyaev <reistlin12@gmail.com>
2023-03-25 15:49:42 +0300

    fix build

--- 14_pushes/app/src/main/java/ru/netology/nmedia/repository/PostRepositoryImpl.kt
+++ 14_pushes/app/src/main/java/ru/netology/nmedia/repository/PostRepositoryImpl.kt
@@ -1,6 +1,6 @@
 package ru.netology.nmedia.repository
 
-import androidx.lifecycle.Transformations
+import androidx.lifecycle.map
 import ru.netology.nmedia.dao.PostDao
 import ru.netology.nmedia.dto.Post
 import ru.netology.nmedia.entity.PostEntity
@@ -8,7 +8,7 @@ import ru.netology.nmedia.entity.PostEntity
 class PostRepositoryImpl(
     private val dao: PostDao,
 ) : PostRepository {
-    override fun getAll() = Transformations.map(dao.getAll()) { list ->
+    override fun getAll() = dao.getAll().map { list ->
         list.map {
             it.toDto()
         }
