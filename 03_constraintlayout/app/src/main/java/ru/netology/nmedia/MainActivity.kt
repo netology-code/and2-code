88db5301091f55eba1378596e11e9b3083dd87cd
Artem Mostyaev <reistlin12@gmail.com>
2025-04-16 12:36:36 +0300

    * upgrade 03_constraintlayout project for the latest AS

--- 03_constraintlayout/app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ 03_constraintlayout/app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -1,16 +1,27 @@
 package ru.netology.nmedia
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
         println(resources.displayMetrics.heightPixels) // 1794
         println(resources.displayMetrics.widthPixels) // 1080
         println(resources.displayMetrics.densityDpi) // 420
         println(resources.displayMetrics.density) // 2.625
+
     }
 }
 
