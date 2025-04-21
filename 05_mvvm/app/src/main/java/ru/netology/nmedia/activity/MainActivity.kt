25342432e1472527deb64557d913ed5d625c579f
Artem Mostyaev <reistlin12@gmail.com>
2025-04-21 14:51:28 +0300

    * upgrade 05_mvvm project for the latest AS

--- 05_mvvm/app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
+++ 05_mvvm/app/src/main/java/ru/netology/nmedia/activity/MainActivity.kt
@@ -1,17 +1,27 @@
 package ru.netology.nmedia.activity
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import ru.netology.nmedia.R
 import ru.netology.nmedia.databinding.ActivityMainBinding
 import ru.netology.nmedia.viewmodel.PostViewModel
 
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
 
         val viewModel: PostViewModel by viewModels()
         viewModel.data.observe(this) { post ->
@@ -19,6 +29,7 @@ class MainActivity : AppCompatActivity() {
                 author.text = post.author
                 published.text = post.published
                 content.text = post.content
+                likeCount.text = post.likes.toString()
                 like.setImageResource(
                     if (post.likedByMe) R.drawable.ic_liked_24 else R.drawable.ic_like_24
                 )
--- 05_mvvm/app/src/main/java/ru/netology/nmedia/repository/PostRepositoryInMemoryImpl.kt
+++ 05_mvvm/app/src/main/java/ru/netology/nmedia/repository/PostRepositoryInMemoryImpl.kt
@@ -10,13 +10,14 @@ class PostRepositoryInMemoryImpl : PostRepository {
         author = "Нетология. Университет интернет-профессий будущего",
         content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
         published = "21 мая в 18:36",
+        likes = 10,
         likedByMe = false
     )
     private val data = MutableLiveData(post)
 
     override fun get(): LiveData<Post> = data
     override fun like() {
-        post = post.copy(likedByMe = !post.likedByMe)
+        post = post.copy(likedByMe = !post.likedByMe, likes = if (post.likedByMe) post.likes - 1 else post.likes + 1)
         data.value = post
     }
 }
