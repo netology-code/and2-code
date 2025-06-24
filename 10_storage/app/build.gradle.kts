54a4de3b76a1c5749161296d082a72b91497e25d
Artem Mostyaev <reistlin12@gmail.com>
2025-06-24 13:14:01 +0300

    * upgrade compileSdk and gradle version

--- 10_storage/app/build.gradle.kts
+++ 10_storage/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "ru.netology.nmedia"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "ru.netology.nmedia"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
