065a39cd4b11e56febcf9c2eabc0f336c1119c5d
Artem Mostyaev <reistlin12@gmail.com>
2025-12-17 11:50:34 +0300

    * update jvmToolchain DSL

--- 01_firstapp/app/build.gradle.kts
+++ 01_firstapp/app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    kotlinOptions {
-        jvmTarget = "11"
+    kotlin {
+        jvmToolchain(11)
     }
 }
 
