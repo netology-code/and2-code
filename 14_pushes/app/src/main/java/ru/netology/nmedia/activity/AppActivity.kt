a103c00ad5ef87ab8cbf61cc2760eab88bcea128
Anatoliy Spitchenko <anatolij.sp@yandex.ru>
2026-02-06 20:32:22 +0700

    Исправление подпрыгивания кнопки создания поста

--- 14_pushes/app/src/main/java/ru/netology/nmedia/activity/AppActivity.kt
+++ 14_pushes/app/src/main/java/ru/netology/nmedia/activity/AppActivity.kt
@@ -30,7 +30,7 @@ class AppActivity : AppCompatActivity() {
         ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+            WindowInsetsCompat.CONSUMED
         }
         requestNotificationsPermission()
 
